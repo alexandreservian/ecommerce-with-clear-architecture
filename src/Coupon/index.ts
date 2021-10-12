9236abcd307f430b03ac0eebaf9c2d6603cff912
alexandreservian <alexandreservian@gmail.com>
2021-10-11 23:28:28 -0300

    feat: added class Coupon

--- src/Coupon/index.ts
+++ src/Coupon/index.ts
@@ -1,5 +1,18 @@
 class Coupon {
-  constructor(readonly name: string, readonly percentage: number) {}
+  constructor(
+    readonly name: string,
+    readonly percentage: number,
+    readonly timestamp: number
+  ) {
+    if (!this.isValid(timestamp)) {
+      throw new Error("Coupon expired");
+    }
+  }
+
+  private isValid(timestamp: number): boolean {
+    const currentDate: number = Math.floor(new Date().getTime() / 1000);
+    return timestamp >= currentDate;
+  }
 }
 
 export default Coupon;
